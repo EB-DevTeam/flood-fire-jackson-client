0a9f074121e14e33e0ee9a0863947316c435c7ae
unknown <emily30198@byui.edu>
2020-07-06 07:34:55 -0600

    image left css

--- src/ui/views/fire/childComp/FireTwoImg.jsx
+++ src/ui/views/fire/childComp/FireTwoImg.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 
-export default function FireTwoImg () {
+export default function FireTwoImg() {
   return (
-    <section className="fireTwoImg__section banner__section-container">
+    <section className="fireTwoImg__section twoImg__section-container">
       <div>
-          <div>
-              <div></div>
-                <div></div>
-          </div>
-          <div>
-              <h2>Fire Repair Services</h2>
-
-          </div>
-          <div>
-
-          </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <h2>Fire Repair Services</h2>
+          <li>Emergency response and board up service</li>
+          <li>Smoke and soot removal</li>
+          <li>Corrosion control</li>
+          <li>Water damage cleanup and restoration</li>
+        </div>
+        <div>
+          <h2></h2>
+          <li>Complete cleaning</li>
+          <li>Decontamination</li>
+          <li>Ozoning</li>
+          <li>Deodorization services</li>
+        </div>
+      </div>
+      <div>
+        <p>
+          Minimize disruption to your home or office. Let Flood & Fire Solutions
+          help you recover from fire damage. Contact us today at (307)215-9741
+          to learn more.
+        </p>
       </div>
     </section>
   );
