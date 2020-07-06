dd9885e221fa84c288e6f215b124b4000602ad80
unknown <emily30198@byui.edu>
2020-07-06 11:25:14 -0600

    nutural disaster

--- src/ui/views/naturalDisaster/childComp/DisasterServices.jsx
+++ src/ui/views/naturalDisaster/childComp/DisasterServices.jsx
@@ -4,32 +4,33 @@ export default function DisasterServices() {
   return (
     <section className="disasterServices__section twoImg__section-container">
       <div className="twoImg__div-gridBox">
-        <div className="disasterServices__img-one"></div>
-        <div className="disasterServices__img-two"></div>
+        <div className="disasterServices__img-one twoImg__img-one"></div>
+        <div className="disasterServices__img-two  twoImg__img-two"></div>
         <div>
           <h2>Natural Disaster Services</h2>
-          <span class="dot twoImg__dot"></span>
-            Emergency response and board up service <br/>
+          <p className="twoImg__p-paddingTop">
             <span class="dot twoImg__dot"></span>
-          Smoke and soot removal <br/>
-          <span class="dot twoImg__dot"></span>
-          Corrosion control <br/>
-          <span class="dot twoImg__dot"></span>
-          Water damage cleanup and restoration <br/>
+            Emergency response and board up service <br />
+            <span class="dot twoImg__dot"></span>
+            Smoke and soot removal <br />
+            <span class="dot twoImg__dot"></span>
+            Corrosion control <br />
+            <span class="dot twoImg__dot"></span>
+            Water damage cleanup and restoration <br />
+          </p>
         </div>
         <div>
-          <br />
-          <br />
-          <br />
-          <br />
-          <span class="dot twoImg__dot"></span>
-          Complete cleaning <br/>
-          <span class="dot twoImg__dot"></span>
-          Decontamination <br/>
-          <span class="dot twoImg__dot"></span>
-          Ozoning <br/>
-          <span class="dot twoImg__dot"></span>
-          Deodorization services <br/>
+          <div className="twoImg__div-space"></div>
+          <p className="twoImg__p-paddingTop">
+            <span class="dot twoImg__dot"></span>
+            Complete cleaning <br />
+            <span class="dot twoImg__dot"></span>
+            Decontamination <br />
+            <span class="dot twoImg__dot"></span>
+            Ozoning <br />
+            <span class="dot twoImg__dot"></span>
+            Deodorization services <br />
+          </p>
         </div>
       </div>
       <div>
