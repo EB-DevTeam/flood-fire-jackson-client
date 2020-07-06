ca6779f2c687be0407e932f4a9d9396647f07a9d
unknown <ber17036@byui.edu>
2020-07-06 08:31:29 -0600

    apps

---
+++ src/ui/views/naturalDisaster/childComp/CraftsMatter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function CraftsMatter() {
+  return (
+    <section>
+      <div className="imageRight__div-cont">
+        <div className="imageRight__div-textCont">
+          <h2 className="imageRight__h2">
+          Craftsmanship Matters
+          </h2>
+          <div className="imageRight__div-colorBox"></div>
+          <p>We are not the typical restoration company.</p>
+          <p className="imageRight__p buttonPadding">Flood & Fire Solutions of Jackson specializes in high end luxury projects. Our professionals approach projects with a keen insight and experience in advanced woodworking and craftsmanship, hydronic heat systems, automated systems and more. Our Superior craftsman take pride in providing the highest quality of service in the Jackson Hole Wyoming.</p>
+          <a className="blue-to-grey__button" href="tel:3072159741"> (307) 215-9741</a>
+        </div>
+        <div className="imageRight__img toolsTrade__img">
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/ui/views/naturalDisaster/childComp/ExperiencedRestoration.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ExperiencedRestoration() {
+  return (
+    <section className="experiencedRestoration__section">
+      <div className="imageRightFlush__div-gridBox experiencedRestoration_gridBox">
+        <div>
+          <h2>Experienced Restoration</h2>
+         <p className="buttonPadding">We’ll take the necessary steps to ensure that your property is safe and secure, such as boarding windows, covering holes or exposed areas with tarps or other materials, limiting unauthorized access with temporary fencing, extracting water and much more.
+          </p>
+        </div>
+        <div className="imageRightFlush__div-img experiencedRestoration_img">
+
+        </div>
+      </div>
+    </section>
+  );
+}
