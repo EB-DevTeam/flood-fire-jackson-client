ef2b8a2677cb12c51a70704349621cbadba5be06
unknown <emily30198@byui.edu>
2020-07-06 10:57:34 -0600

    right image

--- src/ui/views/water/childComp/RestoreServices.jsx
+++ src/ui/views/water/childComp/RestoreServices.jsx
@@ -7,22 +7,34 @@ export default function RestoreServices() {
         <div className="imageLeftFlush__div-img restoreServices_img"></div>
         <div className="imageLeftFlush__div-boxTwo">
           <h2>Water Restoration Services</h2>
-            <p><span class="dot restoreServices__dot"></span>
+          <p>
+            <div className="buttonSpace">
+              <span class="dot restoreServices__dot"></span>
               Identify, control and eliminate the source of the water damage.
-            <br/>
-            <span class="dot restoreServices__dot"></span>Extraction of standing water with high powered equipment.
-            <br/>
-            <span class="dot restoreServices__dot"></span>High-speed drying with strategically placed Air Movers.
-            <br/>
-            <span class="dot restoreServices__dot"></span>
+              <br />
+            </div>
+            <div className="buttonSpace">
+              <span class="dot restoreServices__dot"></span>Extraction of
+              standing water with high powered equipment.
+              <br />
+            </div>
+            <div className="buttonSpace">
+              <span class="dot restoreServices__dot"></span>High-speed drying
+              with strategically placed Air Movers.
+              <br />
+            </div>
+            <div className="buttonSpace">
+              <span class="dot restoreServices__dot"></span>
               Dehumidifying affected areas with state-of-the-art efficient and
               effective equipment.
-              <br/>
+              <br />
+            </div>
+            <div className="buttonSpace">
             <span class="dot restoreServices__dot"></span>
-              Temperature control to provide the most effective drying
-              conditions with TES Evaporator.
-            </p>
-          
+            Temperature control to provide the most effective drying conditions
+            with TES Evaporator.
+            </div>
+          </p>
         </div>
       </div>
     </section>
